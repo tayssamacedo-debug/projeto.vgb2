a277b4abdc943a77b988405e6ec82fa2d8fea196
tayssamacedo-debug <tayssa.macedo@escola.pr.gov.br>
2026-08-10 11:10:13 -0300

    4

---
+++ script.js
@@ -0,0 +1,12 @@
+<article>
+                <img src="
+                "> </img>
+                <div>
+                    <h2>Meu projeto</h2>
+                    <p class="artigo-autor">vick macedo</p>
+                    <p> musica me faz ...</p>
+                    <p> amo Bp...</p>
+            <button>💀 <span>0</span></button>
+            <button>💭 <span>0</span></button>
+                </div>
+            </article>
